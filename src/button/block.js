957c6440044d84e2df9253de846346285ab90e9e
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2018-10-11 10:14:04 -0700

    Update button icon

--- src/button/block.js
+++ src/button/block.js
@@ -37,7 +37,7 @@ const { ServerSideRender, TextControl, SelectControl, ToggleControl } = wp.compo
 registerBlockType( 'mayflower-blocks/button', {
 	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
 	title: __( 'Button' ), // Block title.
-	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+	icon: 'slides', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 
 	attributes: {
