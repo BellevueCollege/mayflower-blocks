57ddf124bd36196daceb9fba62dc2e95e8242b7a
Angela Vu <angela.vu@bellevuecollege.edu>
2018-11-06 16:12:16 -0800

    Fix Masonry Errors
    Moved Masonry scripts to directly enqueue under fluid-grid in the render block function

--- src/child-pages/block.php
+++ src/child-pages/block.php
@@ -18,6 +18,10 @@ function mayflower_blocks_render_block_child_pages( $attributes, $content ) {
 				break;
 			case 'fluid-grid':
 				require get_template_directory() . '/inc/nav-page/fluid-grid.php';
+				//Enqueue scripts directly here when fluid-grid is chosen, so masonry scripts do not error out on other blocks and template options
+				wp_enqueue_script( 'imagesloaded' );
+				wp_enqueue_script( 'masonry' );
+				wp_enqueue_script( 'page-nav-page-fluid-grid', get_template_directory_uri() . '/js/page-nav-page-fluid-grid.js', array( 'wp-blocks', 'wp-element','imagesloaded', 'masonry' ), '', true );
 				break;
 		}
 		
@@ -40,10 +44,13 @@ register_block_type( 'mayflower-blocks/child-pages', array(
 	'render_callback' => 'mayflower_blocks_render_block_child_pages',
 ) );
 
-add_action( 'enqueue_block_assets', 'mayflower_blocks_enqueue_script_child_pages' );
+/*
+* Currently not enqueueing Masonry scripts separately due to being unable to access attributes: $attributes['pageID'] and $attributes['template']
+*/
+// add_action( 'enqueue_block_assets', 'mayflower_blocks_enqueue_script_child_pages' );
 
-function mayflower_blocks_enqueue_script_child_pages () {
-	wp_enqueue_script( 'imagesloaded' );
-	wp_enqueue_script( 'masonry' );
-	wp_enqueue_script( 'page-nav-page-fluid-grid', get_template_directory_uri() . '/js/page-nav-page-fluid-grid.js', array( 'wp-blocks', 'wp-element','imagesloaded', 'masonry' ), '', true );
-}
+// function mayflower_blocks_enqueue_script_child_pages () {
+// 		wp_enqueue_script( 'imagesloaded' );
+// 		wp_enqueue_script( 'masonry' );
+// 		wp_enqueue_script( 'page-nav-page-fluid-grid', get_template_directory_uri() . '/js/page-nav-page-fluid-grid.js', array( 'wp-blocks', 'wp-element','imagesloaded', 'masonry' ), '', true );
+// }
