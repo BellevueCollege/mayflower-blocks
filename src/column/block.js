a94648400e4bef021909755fc4b868424322c14a
Angela Vu <angela.vu@bellevuecollege.edu>
2020-09-07 17:24:05 -0700

    👌 IMPROVE: Columns with Eslint and Inserter Note

--- src/column/block.js
+++ src/column/block.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 /**
  * BLOCK: Column
  *
@@ -12,7 +13,7 @@ import './editor.scss';
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
 const { InspectorControls, InnerBlocks, BlockControls } = wp.editor;
-const { SelectControl, Button, Disabled, Toolbar, Panel, PanelBody, PanelRow, SVG, Path, G} = wp.components;
+const { SelectControl, Button, Disabled, Toolbar, Panel, PanelBody, PanelRow, SVG, Path, G } = wp.components;
 const { select, dispatch } = wp.data;
 const { createHigherOrderComponent } = wp.compose;
 
@@ -34,78 +35,76 @@ const { createHigherOrderComponent } = wp.compose;
 // corresponding bootstrap and CSS classes and prevent block stacking
 const mayflowerBlocksColumn = createHigherOrderComponent( ( BlockListBlock ) => {
 	return ( props ) => {
-		if (props.attributes.gridColumnClass && props.attributes.gridColumnSize){
-			return <BlockListBlock { ...props } className={`col-${props.attributes.gridColumnClass}-${props.attributes.gridColumnSize} ${props.attributes.isEditing == true ? 'mbcolumn-is-editing' : ''} ${props.attributes.isVisible == false ? 'mbcolumn-is-invisible' : ''}`}/>;
-		} else {
-			return <BlockListBlock { ...props } />;
+		if ( props.attributes.gridColumnClass && props.attributes.gridColumnSize ) {
+			return <BlockListBlock { ...props } className={ `col-${ props.attributes.gridColumnClass }-${ props.attributes.gridColumnSize } ${ props.attributes.isEditing === true ? 'mbcolumn-is-editing' : '' } ${ props.attributes.isVisible === false ? 'mbcolumn-is-invisible' : '' }` } />;
 		}
+		return <BlockListBlock { ...props } />;
 	};
 }, 'mayflowerBlocksColumn' );
 
 // Hook the HOC to replace the wrapping div for column blocks
 wp.hooks.addFilter( 'editor.BlockListBlock', 'mayflower-blocks/column', mayflowerBlocksColumn );
 
-registerBlockType('mayflower-blocks/column', {
+registerBlockType( 'mayflower-blocks/column', {
 	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __('Column'), // Block title.
+	title: __( 'Column' ), // Block title.
 	icon: 'schedule', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	category: 'bootstrap-blocks', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
-	parent: ['mayflower-blocks/row'], //Parent Block — Sets the parent block so this block only shows when the parent is being used.
+	parent: [ 'mayflower-blocks/row' ], //Parent Block — Sets the parent block so this block only shows when the parent is being used.
 
 	attributes: {
 		gridColumnClass: {
 			type: 'string',
-			default: 'md'
+			default: 'md',
 		},
 		gridColumnSize: {
 			type: 'number',
-			default: 4
+			default: 4,
 		},
 		isEditing: {
 			type: 'boolean',
-			default: false
+			default: false,
 		},
 		isVisible: {
 			type: 'boolean',
-			default: true
+			default: true,
 		},
 		siblingColumns: {
 			type: 'number',
-			default: 0
+			default: 0,
 		},
 	},
 
-	edit: function ({ className, attributes, setAttributes, clientId }) {
-
+	edit: function( { className, attributes, setAttributes } ) {
 		//Instantiate Column Data
-		const currentBlockClientId = select('core/editor').getSelectedBlockClientId(); // return this block's client id
-		const parentBlockClientId = select('core/editor').getBlockRootClientId(currentBlockClientId); //get parent's client id
-		const parentBlockData = select('core/editor').getBlock(parentBlockClientId); // get parent's data; includes children and attributes
+		const currentBlockClientId = select( 'core/editor' ).getSelectedBlockClientId(); // return this block's client id
+		const parentBlockClientId = select( 'core/editor' ).getBlockRootClientId( currentBlockClientId ); //get parent's client id
+		const parentBlockData = select( 'core/editor' ).getBlock( parentBlockClientId ); // get parent's data; includes children and attributes
 		let parentBlockChildren;
 
 		/**
 		 * ColumnToolBarControl returns a Toolbar component with edit and remove buttons to edit the column or remove the column.
-		 * 
+		 *
 		 * @return Toolbar component with column control buttons
 		 * */
 		const ColumnToolBarControl = () => {
 			const controls = [
 				{
-					icon: <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><rect x="0" fill="none" width="20" height="20"/><G><Path d="M13.89 3.39l2.71 2.72c.46.46.42 1.24.03 1.64l-8.01 8.02-5.56 1.16 1.16-5.58s7.6-7.63 7.99-8.03c.39-.39 1.22-.39 1.68.07zm-2.73 2.79l-5.59 5.61 1.11 1.11 5.54-5.65zm-2.97 8.23l5.58-5.6-1.07-1.08-5.59 5.6z"/></G></SVG>,
+					icon: <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><rect x="0" fill="none" width="20" height="20" /><G><Path d="M13.89 3.39l2.71 2.72c.46.46.42 1.24.03 1.64l-8.01 8.02-5.56 1.16 1.16-5.58s7.6-7.63 7.99-8.03c.39-.39 1.22-.39 1.68.07zm-2.73 2.79l-5.59 5.61 1.11 1.11 5.54-5.65zm-2.97 8.23l5.58-5.6-1.07-1.08-5.59 5.6z" /></G></SVG>,
 					title: 'Edit',
-					onClick: () => handleEditColumnBlock()
+					onClick: () => handleEditColumnBlock(),
 				},
 				{
-					icon: <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><rect x="0" fill="none" width="20" height="20"/><G><Path d="M12 4h3c.6 0 1 .4 1 1v1H3V5c0-.6.5-1 1-1h3c.2-1.1 1.3-2 2.5-2s2.3.9 2.5 2zM8 4h3c-.2-.6-.9-1-1.5-1S8.2 3.4 8 4zM4 7h11l-.9 10.1c0 .5-.5.9-1 .9H5.9c-.5 0-.9-.4-1-.9L4 7z"/></G></SVG>,
+					icon: <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><rect x="0" fill="none" width="20" height="20" /><G><Path d="M12 4h3c.6 0 1 .4 1 1v1H3V5c0-.6.5-1 1-1h3c.2-1.1 1.3-2 2.5-2s2.3.9 2.5 2zM8 4h3c-.2-.6-.9-1-1.5-1S8.2 3.4 8 4zM4 7h11l-.9 10.1c0 .5-.5.9-1 .9H5.9c-.5 0-.9-.4-1-.9L4 7z" /></G></SVG>,
 					title: 'Remove',
-					onClick: () => handleRemoveColumnBlock()
+					onClick: () => handleRemoveColumnBlock(),
 				},
 			];
 
-			return(
-				<Toolbar controls = { controls } />
+			return (
+				<Toolbar controls={ controls } />
 			);
-		}
+		};
 
 		/**
 		 * Removes the column block
@@ -113,34 +112,33 @@ registerBlockType('mayflower-blocks/column', {
 		 * When a column block gets removed, the function also adjusts the width of each column block under the row block to fit each new column
 		 */
 		const handleRemoveColumnBlock = () => {
-			dispatch('core/editor').removeBlock(currentBlockClientId, false);
-			dispatch('core/editor').updateBlockAttributes(parentBlockClientId, { childColumns: attributes.siblingColumns - 1 }); // Updates the parent
+			dispatch( 'core/editor' ).removeBlock( currentBlockClientId, false );
+			dispatch( 'core/editor' ).updateBlockAttributes( parentBlockClientId, { childColumns: attributes.siblingColumns - 1 } ); // Updates the parent
 
-			const parentBlockDataChildren = select('core/editor').getBlock(parentBlockClientId).innerBlocks; // get parent's children
+			const parentBlockDataChildren = select( 'core/editor' ).getBlock( parentBlockClientId ).innerBlocks; // get parent's children
 
-			if (Array.isArray(parentBlockDataChildren)) {
+			if ( Array.isArray( parentBlockDataChildren ) ) {
 				//Performs a check on all siblings to see how many columns there are and sets the sibling columns size to adjust to the newly removed column
-				parentBlockDataChildren.forEach(sibling => {
+				parentBlockDataChildren.forEach( sibling => {
 					//update all siblings that a sibling has been removed
-					dispatch('core/editor').updateBlockAttributes(sibling.clientId, { siblingColumns: attributes.siblingColumns - 1 });
+					dispatch( 'core/editor' ).updateBlockAttributes( sibling.clientId, { siblingColumns: attributes.siblingColumns - 1 } );
 
 					// set the column size if children is divisible equally by 12 columns, else set it to size of 4 (5 columns)
-					if ( 12 % (attributes.siblingColumns - 1) == 0 ) { // sets size for 1, 2, 3, 4, 6 columns
-						dispatch('core/editor').updateBlockAttributes(sibling.clientId, { gridColumnSize: ( 12 / ( attributes.siblingColumns - 1 ) ) });
-					} else {
-						//if a sibling is the 5th column after the 6th column is removed, then update the attributes to the new gridColumnSize
-						if (sibling.clientId == parentBlockDataChildren[parentBlockDataChildren.length - 1].clientId) {
-							dispatch('core/editor').updateBlockAttributes(parentBlockDataChildren[parentBlockDataChildren.length - 1].clientId, { gridColumnSize: 4 });
-						}
+					if ( 12 % ( attributes.siblingColumns - 1 ) === 0 ) { // sets size for 1, 2, 3, 4, 6 columns
+						dispatch( 'core/editor' ).updateBlockAttributes( sibling.clientId, { gridColumnSize: ( 12 / ( attributes.siblingColumns - 1 ) ) } );
 					}
-				});
+					//if a sibling is the 5th column after the 6th column is removed, then update the attributes to the new gridColumnSize
+					if ( sibling.clientId === parentBlockDataChildren[ parentBlockDataChildren.length - 1 ].clientId ) {
+						dispatch( 'core/editor' ).updateBlockAttributes( parentBlockDataChildren[ parentBlockDataChildren.length - 1 ].clientId, { gridColumnSize: 4 } );
+					}
+				} );
 			}
 
 			//If a column is removed while editing, also close the column
-			if (attributes.isEditing == true) {
+			if ( attributes.isEditing === true ) {
 				handleCloseColumnBlock();
 			}
-		}
+		};
 
 		/**
 		 * Selects a column block to edit content
@@ -149,21 +147,21 @@ registerBlockType('mayflower-blocks/column', {
 		 * On click it will render the column to full-width.
 		 */
 		const handleEditColumnBlock = () => {
-			setAttributes({ isEditing: true });
-			
+			setAttributes( { isEditing: true } );
+
 			parentBlockChildren = parentBlockData.innerBlocks;
 
-			if (Array.isArray(parentBlockChildren)) {
+			if ( Array.isArray( parentBlockChildren ) ) {
 				// If each sibling is not the current child, then set visibility to false
-				parentBlockChildren.forEach(sibling => {
-					if (sibling.clientId != currentBlockClientId) {
-						dispatch('core/editor').updateBlockAttributes(sibling.clientId, { isVisible: false });
+				parentBlockChildren.forEach( sibling => {
+					if ( sibling.clientId !== currentBlockClientId ) {
+						dispatch( 'core/editor' ).updateBlockAttributes( sibling.clientId, { isVisible: false } );
 					}
-				});
+				} );
 			}
 
 			// Tell parent a child is selected for editing
-			dispatch('core/editor').updateBlockAttributes(parentBlockClientId, { childIsEditing: true });
+			dispatch( 'core/editor' ).updateBlockAttributes( parentBlockClientId, { childIsEditing: true } );
 		};
 
 		/**
@@ -173,48 +171,48 @@ registerBlockType('mayflower-blocks/column', {
 		 * isVisible to true and if selected it updates isEditing to false.
 		 */
 		const handleCloseColumnBlock = () => {
-			setAttributes({ isEditing: false });
+			setAttributes( { isEditing: false } );
 
 			parentBlockChildren = parentBlockData.innerBlocks;
 
-			if (Array.isArray(parentBlockChildren)) {
+			if ( Array.isArray( parentBlockChildren ) ) {
 				// If each sibling is not the current child, then set visibility to true
-				parentBlockChildren.forEach(sibling => {
-					if (sibling.clientId != currentBlockClientId) {
-						dispatch('core/editor').updateBlockAttributes(sibling.clientId, { isVisible: true });
+				parentBlockChildren.forEach( sibling => {
+					if ( sibling.clientId !== currentBlockClientId ) {
+						dispatch( 'core/editor' ).updateBlockAttributes( sibling.clientId, { isVisible: true } );
 					}
-				});
+				} );
 			}
 
 			// tell parent child is no longer selected
-			dispatch('core/editor').updateBlockAttributes(parentBlockClientId, { childIsEditing: false });
-		}
+			dispatch( 'core/editor' ).updateBlockAttributes( parentBlockClientId, { childIsEditing: false } );
+		};
 
 		return [
 			<InspectorControls>
-				<Panel> 
+				<Panel>
 					<PanelBody
 						title="Column Controls"
 						opened={ true }
 					>
 						<SelectControl
 							label="Column Class"
-							value={attributes.gridColumnClass}
-							options={[
+							value={ attributes.gridColumnClass }
+							options={ [
 								{ label: 'X-Small (XS Mobile)', value: 'xs' },
 								{ label: 'Small (Mobile)', value: 'sm' },
 								{ label: 'Medium (Tablet)', value: 'md' },
 								{ label: 'Large (Desktop)', value: 'lg' },
-							]}
-							onChange={(gridColumnClass) => {
-								setAttributes({ gridColumnClass });
-							}}
+							] }
+							onChange={ ( gridColumnClass ) => {
+								setAttributes( { gridColumnClass } );
+							} }
 						/>
 
 						<SelectControl
 							label="Column Size"
-							value={attributes.gridColumnSize}
-							options={[
+							value={ attributes.gridColumnSize }
+							options={ [
 								{ label: '1', value: 1 },
 								{ label: '2', value: 2 },
 								{ label: '3', value: 3 },
@@ -227,53 +225,49 @@ registerBlockType('mayflower-blocks/column', {
 								{ label: '10', value: 10 },
 								{ label: '11', value: 11 },
 								{ label: '12', value: 12 },
-							]}
-							onChange={(gridColumnSize) => {
+							] }
+							onChange={ ( gridColumnSize ) => {
 								// Parse to int because it gets casted as a string
-								let size = parseInt(gridColumnSize, 10); 
-								setAttributes({ gridColumnSize: size });
-							}}
+								const size = parseInt( gridColumnSize, 10 );
+								setAttributes( { gridColumnSize: size } );
+							} }
 						/>
 
-						{attributes.isEditing == true ?
+						{ attributes.isEditing === true ?
 							<PanelRow>
-								<Button isDefault onClick={handleCloseColumnBlock}>
+								<Button isDefault onClick={ handleCloseColumnBlock }>
 									Save &amp; Close Column
 								</Button>
-							</PanelRow>
-							:
+							</PanelRow>							:
 							<PanelRow>
-								<Button isDefault onClick={handleEditColumnBlock}>
+								<Button isDefault onClick={ handleEditColumnBlock }>
 									Edit Column
 								</Button>
 							</PanelRow>
 						}
 						<PanelRow>
-							<Button isDefault onClick={handleRemoveColumnBlock}>
+							<Button isDefault onClick={ handleRemoveColumnBlock }>
 								Remove Column
 							</Button>
 						</PanelRow>
 					</PanelBody>
 				</Panel>
-			</InspectorControls>
-			,
-			attributes.isEditing == false ? 
-			<BlockControls>
-				<ColumnToolBarControl/>
-			</BlockControls> : ''
-			,
-			<div className={className}>
+			</InspectorControls>,
+			attributes.isEditing === false ?
+				<BlockControls>
+					<ColumnToolBarControl />
+				</BlockControls> : '',
+			<div className={ className }>
 				{ attributes.gridColumnClass &&
 					attributes.isEditing !== true ?
-						<Disabled>
-							<InnerBlocks />
-						</Disabled>
-					: <InnerBlocks />
+					<Disabled>
+						<InnerBlocks />
+					</Disabled> :
+					<InnerBlocks />
 				}
-			</div>
-		]
+			</div>,
+		];
 	},
-
 
 	/**
 	 * The save function defines the way in which the different attributes should be combined
@@ -284,10 +278,9 @@ registerBlockType('mayflower-blocks/column', {
 	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
 	 */
 
-	save: function ({ attributes }) {
-
+	save: function( { attributes } ) {
 		return (
-			<div className={`col-${attributes.gridColumnClass}-${attributes.gridColumnSize}`}>
+			<div className={ `col-${ attributes.gridColumnClass }-${ attributes.gridColumnSize }` }>
 				<InnerBlocks.Content />
 			</div>
 		);
@@ -305,9 +298,9 @@ registerBlockType('mayflower-blocks/column', {
 					// Grid Column Text
 					gridColumnText: {
 						type: 'string',
-						shortcode: (attrs, { content }) => {
+						shortcode: ( attrs, { content } ) => {
 							// Content returns the whole shortcode, so we need to match only shortcode content
-							let filtered = content.replace(/(\[\s*column\s*.*?\]\s*)|(\s*\[\s*\/\s*column\s*\])/gmi, '');
+							const filtered = content.replace( /(\[\s*column\s*.*?\]\s*)|(\s*\[\s*\/\s*column\s*\])/gmi, '' );
 
 							// Return filtered content if there was a match, otherwise return blank string
 							return filtered ? filtered : '';
@@ -317,30 +310,29 @@ registerBlockType('mayflower-blocks/column', {
 					// Grid Column Class
 					gridColumnClass: {
 						type: 'string',
-						shortcode: (attrs, { content }) => {
-							let columnRx = /(\w{2})(?=\s*=)/gmi;
-							let filtered = content.match(columnRx);
-							return Array.isArray(filtered) ? filtered[0] : '';
+						shortcode: ( attrs, { content } ) => {
+							const columnRx = /(\w{2})(?=\s*=)/gmi;
+							const filtered = content.match( columnRx );
+							return Array.isArray( filtered ) ? filtered[ 0 ] : '';
 						},
 					},
 
 					gridColumnSize: {
 						type: 'string',
-						shortcode: ({ named: { xs, sm, md, lg } }) => {
-							if (xs) {
+						shortcode: ( { named: { xs, sm, md, lg } } ) => {
+							if ( xs ) {
 								return xs;
-							} else if (sm) {
+							} else if ( sm ) {
 								return sm;
-							} else if (md) {
+							} else if ( md ) {
 								return md;
-							} else {
-								return lg;
 							}
+							return lg;
 						},
 					},
 				},
-			}
-		]
+			},
+		],
 	},
-});
+} );
 
