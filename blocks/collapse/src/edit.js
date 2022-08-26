f4bcbce509ef2045512a5e09a0654b3d5ea71278
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2022-08-26 15:44:24 -0700

    👌 IMPROVE: Collapse Block Controls
    Add Toolbar Controls for all options

--- blocks/collapse/src/edit.js
+++ blocks/collapse/src/edit.js
@@ -2,43 +2,37 @@
  * Block: Collapse (Parent block of Collapsibles)
  */
 
- import { __ } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
+
+import {
+	SelectControl,
+	ToggleControl,
+	Toolbar,
+	ToolbarDropdownMenu,
+	ToolbarButton,
+	SVG,
+	Path,
+	G,
+	PanelBody,
+	PanelRow,
+	Tooltip
+} from '@wordpress/components';
+
+import {
+	useBlockProps,
+	RichText,
+	BlockControls,
+	InspectorControls,
+	InnerBlocks,
+} from '@wordpress/block-editor';
 
 
- import { useEffect, useState, useRef } from '@wordpress/element';
+import './editor.scss';
+import './style.scss';
+import {select,} from '@wordpress/data';
 
- import {
-	 TextControl,
-	 SelectControl,
-	 ToggleControl,
-	 ToolbarButton,
-	 Popover,
-	 ToolbarDropdownMenu,
-	 Toolbar,
-	 SVG,
-	 Path,
-	 G,
-	 PanelBody,
-	 PanelRow
- } from '@wordpress/components';
-
- import {
-	 useBlockProps,
-	 RichText,
-	 BlockControls,
-	 InspectorControls,
-	 AlignmentToolbar,
-	 getBlockRootClientId,
-	 InnerBlocks,
- } from '@wordpress/block-editor';
-
-
- import './editor.scss';
- import './style.scss';
- import {select,} from '@wordpress/data';
-
- export default function Edit( props ) {
-	 const { attributes: {
+export default function Edit( props ) {
+	const { attributes: {
 		collapseHeadingText,
 		collapseText,
 		collapseClass,
@@ -47,19 +41,24 @@
 		parentBlockClientId,
 		collapseLightBg,
 		headingTag,
-	 }, setAttributes, isSelected, clientId } = props;
-	 const blockProps = useBlockProps({
+	}, setAttributes, isSelected, clientId } = props;
+
+	const blockProps = useBlockProps({
 		className: 'card bg-' + collapseClass + (
 			collapseClass !== 'default' &&
 			collapseClass !== 'light' &&
-			collapseClass !== 'info' ? ' text-white' : '' ) 
-	 });
-	 const HeadingStyleControl = () => {
-		function createClassControl( headingStyle ) {
+			collapseClass !== 'info' ? ' text-white' : '' )
+	});
+	/**
+	 * Select Heading Level from List of Options
+	 */
+	const HeadingStyleControl = () => {
+		function createHeadingControl( headingStyle ) {
 			// get the Toolbar control style name and output the corresponding HTML tag
-			const style = ( 'h' + headingStyle[ headingStyle.length - 1 ] );
+			const style = ( headingStyle == 'Paragraph' ? 'p' : 'h' + headingStyle[ headingStyle.length - 1 ] );
 
 			// save the SVGs
+			const svgHeading = <SVG xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><Path d="M12.5 4v5.2h-5V4H5v13h2.5v-5.2h5V17H15V4"></Path></SVG>;
 			const levelToPath = {
 				1: <SVG xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><Path d="M9 5h2v10H9v-4H5v4H3V5h2v4h4V5zm6.6 0c-.6.9-1.5 1.7-2.6 2v1h2v7h2V5h-1.4z"></Path></SVG>,
 				2: <SVG xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><Path d="M7 5h2v10H7v-4H3v4H1V5h2v4h4V5zm8 8c.5-.4.6-.6 1.1-1.1.4-.4.8-.8 1.2-1.3.3-.4.6-.8.9-1.3.2-.4.3-.8.3-1.3 0-.4-.1-.9-.3-1.3-.2-.4-.4-.7-.8-1-.3-.3-.7-.5-1.2-.6-.5-.2-1-.2-1.5-.2-.4 0-.7 0-1.1.1-.3.1-.7.2-1 .3-.3.1-.6.3-.9.5-.3.2-.6.4-.8.7l1.2 1.2c.3-.3.6-.5 1-.7.4-.2.7-.3 1.2-.3s.9.1 1.3.4c.3.3.5.7.5 1.1 0 .4-.1.8-.4 1.1-.3.5-.6.9-1 1.2-.4.4-1 .9-1.6 1.4-.6.5-1.4 1.1-2.2 1.6V15h8v-2H15z"></Path></SVG>,
@@ -68,18 +67,85 @@
 				5: <SVG xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><Path d="M12.1 12.2c.4.3.7.5 1.1.7.4.2.9.3 1.3.3.5 0 1-.1 1.4-.4.4-.3.6-.7.6-1.1 0-.4-.2-.9-.6-1.1-.4-.3-.9-.4-1.4-.4H14c-.1 0-.3 0-.4.1l-.4.1-.5.2-1-.6.3-5h6.4v1.9h-4.3L14 8.8c.2-.1.5-.1.7-.2.2 0 .5-.1.7-.1.5 0 .9.1 1.4.2.4.1.8.3 1.1.6.3.2.6.6.8.9.2.4.3.9.3 1.4 0 .5-.1 1-.3 1.4-.2.4-.5.8-.9 1.1-.4.3-.8.5-1.3.7-.5.2-1 .3-1.5.3-.8 0-1.6-.1-2.3-.4-.6-.2-1.1-.6-1.6-1-.1-.1 1-1.5 1-1.5zM9 15H7v-4H3v4H1V5h2v4h4V5h2v10z"></Path></SVG>,
 				6: <SVG xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><Path d="M9 15H7v-4H3v4H1V5h2v4h4V5h2v10zm8.6-7.5c-.2-.2-.5-.4-.8-.5-.6-.2-1.3-.2-1.9 0-.3.1-.6.3-.8.5l-.6.9c-.2.5-.2.9-.2 1.4.4-.3.8-.6 1.2-.8.4-.2.8-.3 1.3-.3.4 0 .8 0 1.2.2.4.1.7.3 1 .6.3.3.5.6.7.9.2.4.3.8.3 1.3s-.1.9-.3 1.4c-.2.4-.5.7-.8 1-.4.3-.8.5-1.2.6-1 .3-2 .3-3 0-.5-.2-1-.5-1.4-.9-.4-.4-.8-.9-1-1.5-.2-.6-.3-1.3-.3-2.1s.1-1.6.4-2.3c.2-.6.6-1.2 1-1.6.4-.4.9-.7 1.4-.9.6-.3 1.1-.4 1.7-.4.7 0 1.4.1 2 .3.5.2 1 .5 1.4.8 0 .1-1.3 1.4-1.3 1.4zm-2.4 5.8c.2 0 .4 0 .6-.1.2 0 .4-.1.5-.2.1-.1.3-.3.4-.5.1-.2.1-.5.1-.7 0-.4-.1-.8-.4-1.1-.3-.2-.7-.3-1.1-.3-.3 0-.7.1-1 .2-.4.2-.7.4-1 .7 0 .3.1.7.3 1 .1.2.3.4.4.6.2.1.3.3.5.3.2.1.5.2.7.1z"></Path></SVG>,
 			};
-			// check if the heading style is Paragraph or a Heading, and return the corresponding toolbar object
-
 			return {
 				icon: levelToPath[ style.charAt( 1 ) ],
-				title: headingStyle,
+				label: headingStyle,
 				isActive: headingTag === style,
 				onClick: () => setAttributes( { headingTag: style } ),
+				role: 'menuitemradio',
 			};
 		}
 
 		return (
-			<Toolbar controls={ [ 'Heading 2', 'Heading 3', 'Heading 4', 'Heading 5', 'Heading 6' ].map( createClassControl ) } />
+			<ToolbarDropdownMenu
+				icon='heading'
+				label={ __( 'Heading Level' ) }
+				controls={ [ 'Heading 2', 'Heading 3', 'Heading 4', 'Heading 5', 'Heading 6' ].map( createHeadingControl ) }
+			/>
+		);
+	};
+		/**
+	 * ButtonClassControl returns a Toolbar component with alert classes that changes via on click and updates the alert block's style.
+	 *
+	 * @return Toolbar component with alert classes
+	 * */
+	const StyleControl = () => {
+		function createColorControl( colorClass ) {
+			//Switch checks the class control alertClass and returns the corresponding colorClass to update the SVG icon
+			let color = '';
+			switch ( colorClass ) {
+				case 'default':
+					color = '#dddddd';
+					break;
+				case 'primary':
+					color = '#003D79';
+					break;
+				case 'secondary':
+					color = '#6c757d';
+					break;
+				case 'info':
+					color = '#afd7ff';
+					break;
+				case 'success':
+					color = '#317131';
+					break;
+				case 'warning':
+					color = '#F2C01E';
+					break;
+				case 'danger':
+					color = '#C4122F';
+					break;
+				case 'light':
+					color = '#f8f9fa';
+					break;
+				case 'dark':
+					color = '#343a40';
+					break;
+				default:
+					color = '#31708f';
+					break;
+			}
+
+			return {
+				icon: <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+					<G>
+						<Path fill={ color } d="M21.125,2H4.875C2.182,2,0,4.182,0,6.875v12.25C0,21.818,2.182,24,4.875,24h16.25,C23.818,24,26,21.818,26,19.125V6.875C26,4.182,23.818,2,21.125,2z" />
+					</G>
+				</SVG>,
+				title: colorClass.charAt( 0 ).toUpperCase() + colorClass.slice( 1 ),
+				isActive: collapseClass === colorClass,
+				onClick: () => setAttributes( { collapseClass: colorClass } ),
+			};
+		}
+
+		return (
+			<ToolbarDropdownMenu
+				label="Color"
+				icon="color-picker"
+				controls={
+					[ 'default', 'primary', 'secondary', 'info', 'success', 'warning', 'danger', 'light', 'dark' ]
+					.map( createColorControl )
+				} />
 		);
 	};
 	const parentClientId = select('core/block-editor').getBlockRootClientId( clientId );
@@ -87,10 +153,23 @@
 	setAttributes( { currentBlockClientId: clientId } );
 	setAttributes( { parentBlockClientId: parentClientId } );
 	const HeadingTag = headingTag;
-	 return (
-		 <>
+	return (
+		<>
 			<BlockControls>
 				<HeadingStyleControl />
+				<StyleControl />
+				<ToolbarButton
+					icon="lightbulb"
+					label="Light Background"
+					onClick={ () => setAttributes( { collapseLightBg: ! collapseLightBg } ) }
+					isActive={ collapseLightBg }
+				/>
+				<ToolbarButton
+					icon="editor-expand"
+					label="Start Expanded"
+					onClick={ () => setAttributes( { expanded: ! expanded } ) }
+					isActive={ expanded }
+				/>
 			</BlockControls>
 			<InspectorControls>
 				<PanelBody
@@ -99,7 +178,7 @@
 				>
 					<PanelRow>
 						<SelectControl
-							label="Collapse Style"
+							label="Color"
 							value={ collapseClass }
 							options={ [
 								{ label: 'Default', value: 'default' },
