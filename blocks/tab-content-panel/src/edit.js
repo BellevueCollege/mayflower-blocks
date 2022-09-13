8c3ecf26095a3f43ee47397fbbf767644a5200a9
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2022-09-13 11:38:27 -0700

    🐛 FIX: Block Inserter within Tabs
    Resolve [sc-3600] by removing Higher Order Component and letting the
    block inserter do its job as normal.

--- blocks/tab-content-panel/src/edit.js
+++ blocks/tab-content-panel/src/edit.js
@@ -4,46 +4,33 @@
 
 import { __ } from '@wordpress/i18n';
 
-import { Fragment } from '@wordpress/element';
-
-import { createHigherOrderComponent } from '@wordpress/compose';
-
-
 import {
 	useBlockProps,
 	InnerBlocks
 } from '@wordpress/block-editor';
 
-
-// Creates a higher order component(HOC) to properly wrap the tab content panel block with the
-// corresponding bootstrap and CSS classes and prevent block stacking
-const mayflowerBlocksPanel = createHigherOrderComponent( ( BlockListBlock ) => {
-	return ( props ) => {
-		if ( props.attributes.tabId && props.name === 'mayflower-blocks/tab-content-panel' ) {
-			return <div role="tabpanel" className={ `tab-pane ${ props.attributes.tabActive ? 'active' : '' }` } id={ props.attributes.tabId }><BlockListBlock { ...props } /></div>;
-		}
-		return <BlockListBlock { ...props } />;
-	};
-}, 'mayflowerBlocksPanel' );
-
-// Hook the HOC to replace the wrapping div for tab content panel blocks
-wp.hooks.addFilter( 'editor.BlockListBlock', 'mayflower-blocks/tab-content-panel', mayflowerBlocksPanel );
-
-
 export default function Edit( props ) {
-	const blockProps = useBlockProps();
+	const blockProps = useBlockProps(
+		{
+			role: 'tabpanel',
+			className: `tab-pane ${ props.attributes.tabActive ? 'active' : '' }`,
+			id: props.attributes.tabId
+		}
+	);
 	const { attributes: {
 
 	}, isSelected, clientId } = props;
 
 
 	return (
-		<InnerBlocks
-			templateLock={ false }
-			template={ [[ 'core/paragraph', {} ] ] }
-			renderAppender={ InnerBlocks.DefaultBlockAppender }
-			placeholder={ <p>Click the + icon to add a block</p> }
-		/>
+		<div { ...blockProps }>
+			<InnerBlocks
+				templateLock={ false }
+				template={ [[ 'core/paragraph', {} ] ] }
+				renderAppender={ InnerBlocks.DefaultBlockAppender }
+				placeholder={ <p>Click the + icon to add a block</p> }
+			/>
+		</div>
 	);
 
 }
