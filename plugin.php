f50c2cd7b35b9d04e2e03a5d250e46bd7b709177
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2023-10-05 07:39:19 -0700

    Bump Version to 3.4

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Companion Gutenberg Blocks for BC Mayflower Theme
  * Author: BC Integration (Taija, Angela, Elizabeth)
  * Author URI: https://www.bellevuecollege.edu
- * Version: 3.4.0-dev1 #{versionStamp}#
+ * Version: 3.4.0 #{versionStamp}#
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
