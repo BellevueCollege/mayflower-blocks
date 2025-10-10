aaf3d0255d5b06414910f4782d1bb89fbcd8a92f
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2025-10-10 11:45:42 -0700

    👌 IMPROVE: Upgrade Course Block Style
    Include direct links to HCX and Acalog instead of relying on transitional page

--- src/course/class-course.php
+++ src/course/class-course.php
@@ -1,36 +1,39 @@
 <?php
 
 class Mayflower_Blocks_Course {
-	private $link_base_url = 'https://www.bellevuecollege.edu/classes/All/';
-	private $api_base_url = 'https://www2.bellevuecollege.edu/data/api/v1/course/';
+	private $link_base_url        = 'https://www.bellevuecollege.edu/classes/All/';
+	private $dataapi_base_url     = 'https://www2.bellevuecollege.edu/data/api/v1/course/';
+	private $catalog_base_url     = false;
+	private $catalog_base_api_url = false;
+	private $catalog_api_key      = false;
 	private $subject;
 	private $number;
 	private $description;
 	private $headingTag;
-	private $data;
 
 	function __construct( $subject, $number, $description, $headingTag ) {
 		$this->subject = $subject;
 		$this->number = $number;
 		$this->description = $description;
 		$this->headingTag = $headingTag;
+
+		$this->catalog_base_url     = defined( 'ACALOG_BASE_URL' )     ? ACALOG_BASE_URL     : false;
+		$this->catalog_base_api_url = defined( 'ACALOG_BASE_API_URL' ) ? ACALOG_BASE_API_URL : false;
+		$this->catalog_api_key      = defined( 'ACALOG_API_KEY' )      ? ACALOG_API_KEY      : false;
 	}
 
 	/**
-	 * Fetch Course JSON data from API
+	 * Fetch Course Data from Data API in JSON format
 	 */
-	private function fetch_json() {
+	private function dataapi_fetch_json() {
 
-		$key = esc_attr( 'course-' . $this->subject . $this->number );
-
+		$key = esc_attr( 'mfblocks-course-' . $this->subject . $this->number );
 		$raw_data = get_site_transient( $key );
 
 		if ( ! $raw_data ) {
-
 			// Build request URL
 			// Example: https://www.bellevuecollege.edu/data/api/v1/course/ACCT/101
-			$api_url = $this->api_base_url . urlencode( $this->subject ) . '/'. urlencode( $this->number );
-
+			$api_url = $this->dataapi_base_url . urlencode( $this->subject ) . '/'. urlencode( $this->number );
 
 			// Get raw data from API
 			$raw_data = wp_remote_get( $api_url );
@@ -41,25 +44,24 @@ class Mayflower_Blocks_Course {
 
 		}
 
-
 		// Return JSON if successful, otherwise return false
 		return is_array( $raw_data ) ? json_decode( $raw_data['body'] ) : false;
 	}
 
 	/**
-	 * Load Course Information and Save to $data
+	 * Load Course Information from Data API
 	 */
-	private function load_course() {
+	private function dataapi_load_course() {
 
 		// Fetch JSON
-		$raw = $this->fetch_json();
+		$raw = $this->dataapi_fetch_json();
 
 		// Make sure that data has been returned
 		if ( isset( $raw->course ) ) {
 			// Build model
 			$course = $raw->course;
 
-			$this->data = Array(
+			return Array(
 				'title' => $course->title,
 				'subject' => $course->subject,
 				'number' => $course->courseNumber,
@@ -72,18 +74,97 @@ class Mayflower_Blocks_Course {
 	}
 
 	/**
+	 * Determine Current Catalog ID
+	 */
+	private function catalog_current_id( $base_url, $key ) {
+		$url = $base_url . "/content?key=$key&format=xml&method=getCatalogs";
+
+		$catalog_id = get_site_transient( 'mfblocks-catalog-id' );
+
+		if ( $catalog_id ) {
+			return $catalog_id;
+		}
+
+		$xml = simplexml_load_file( "$url" ) or die( "Error: Cannot create object" );
+		foreach ( $xml[0]->catalog  as $catalog ) {
+			if ( $catalog->state->published == 'Yes' && $catalog->state->archived == 'No' ) {
+				$catalog_id = str_replace( 'acalog-catalog-', '', $catalog->attributes()->id );
+			}
+		}
+
+		set_site_transient( 'mfblocks-catalog-id', $catalog_id, ( 4 * HOUR_IN_SECONDS ) + rand( 0, 1 * HOUR_IN_SECONDS ) );
+
+		return $catalog_id;
+	}
+
+	/**
+	 * Fetch Course Data from Acalog API in SOAP format
+	 */
+
+	function catalog_course_id( $base_url, $key, $course_subject, $course_number, $catalog ) {
+		$course_subject = urlencode( '"' . $course_subject . '"' );
+		$course_number = urlencode( '"' . $course_number . '"' );
+		if ( ! $course_subject || ! $course_number || ! $catalog ) {
+			error_log( 'MFBLOCKS: Missing course subject, course number, or catalog' . $course_subject . ' ' . $course_number . ' ' . $catalog );
+			return false;
+		}
+		$transient_name = sanitize_key( 'mfblocks-catalog-course-id-'. base64_encode( $catalog . $course_subject . $course_number ) );
+
+		$course_id = get_transient( $transient_name );
+
+		if ( $course_id ) {
+			return $course_id;
+		} else {
+
+			$url = $base_url . '/search/courses?key=' .
+			$key . '&format=xml&method=search&catalog=' .
+			$catalog . '&query=' .
+			$course_subject . '%20' . $course_number . '&options[sort]=rank&options[limit]=1';
+
+			$request = wp_safe_remote_get( $url );
+			$request_body = wp_remote_retrieve_body( $request );
+			if ( is_wp_error( $request_body ) ) { return false; }
+
+			$xml = simplexml_load_string( $request_body );
+
+			// If no results, return false
+			if ( ! $xml->search->results->result->id ) {
+				error_log( 'CS2HCX: No results for course ' . urldecode($course_subject) . ' ' . urldecode($course_number) . ' (Catalog: ' . urldecode($catalog) . ')' );
+				return false;
+			}
+
+			// If results, return the course ID after setting the transient
+			$course_id = (string)$xml->search->results->result->id;
+			set_transient( $transient_name, $course_id, ( 4 * HOUR_IN_SECONDS ) + rand( 0, 1 * HOUR_IN_SECONDS ) );
+			return $course_id;
+
+		}
+	}
+	/**
 	 * Load and Output Data
 	 */
 	public function output() {
-		$this->load_course();
 
-		$course_data = $this->data;
+		if ( $this->catalog_base_api_url && $this->catalog_api_key ) {
+			$catalog_id        = $this->catalog_current_id( $this->catalog_base_api_url, $this->catalog_api_key );
+			$catalog_course_id = $this->catalog_course_id( $this->catalog_base_api_url, $this->catalog_api_key, $this->subject, $this->number, $catalog_id );
+		}
+
+		if ( $this->catalog_base_url && $catalog_id && $catalog_course_id ) {
+			$catalog_url = esc_url( $this->catalog_base_url . "/preview_course_nopop.php?catoid=$catalog_id&coid=$catalog_course_id" );
+		} else {
+			$catalog_url = false;
+		}
+
+		$hcx_url = esc_url( "https://csprd.ctclink.us/psc/csprd/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_COURSE_CATALOG.FieldFormula.IScript_Main/subjects/{$this->subject}/" . '?institution=WA080');
+
+
+
+		$course_data = $this->dataapi_load_course();
 
 		if ( $course_data ) { //if there is course data, return course information
-			$title = $course_data['subject'] . ' '
-			. $course_data['number'] . ': '
-			. $course_data['title'] . ' - '
-			. ( $course_data['variable'] ? 'variable' : $course_data['credits'] ) . ' credits';
+			$title = "<strong>{$course_data['subject']} {$course_data['number']}:</strong> {$course_data['title']} <span class='badge text-bg-secondary badge-secondary'>" .
+				( $course_data['variable'] ? 'variable' : $course_data['credits'] ) . ' cr.</span>';
 
 			$url = $this->link_base_url . str_replace( '&', '', $course_data['subject'] ) .
 				( $course_data['common'] ? '%26' : '' ) . '/' .
@@ -91,13 +172,19 @@ class Mayflower_Blocks_Course {
 
 			$description = $course_data['description'];
 
-			$more = 'View details for ' . $course_data['subject'] . ' '
-				. $course_data['number'];
+			$catalog_text = 'View ' . $course_data['subject'] . ' '
+				. $course_data['number'] . ' in the Academic Catalog';
+
+			$catalog_link = $catalog_url ? "<a class='btn btn-primary btn-sm ms-1 ml-1 mb-1' href='$catalog_url'>$catalog_text</a>" : '';
+
+			$hcx_text = 'Explore ' . $course_data['subject'] . ' courses in ctcLink';
+
+			$hcx_link = $hcx_url ? "<a class='btn btn-outline-dark btn-sm ms-1 ml-1 mb-1' href='$hcx_url'>$hcx_text</a>" : '';
 
 			if ( $this->description ) {
-				return "<$this->headingTag><a href='$url'>$title</a></$this->headingTag><p>$description</p><p><a href='$url'>$more</a></p>";
+				return "<$this->headingTag>$title</$this->headingTag><p>$description</p><p>$catalog_link $hcx_link</p><hr>";
 			} else {
-				return "<$this->headingTag><a href='$url'>$title</a></$this->headingTag>";
+				return "<$this->headingTag>$title</$this->headingTag><p>$catalog_link $hcx_link</p>";
 			}
 
 		} else {
